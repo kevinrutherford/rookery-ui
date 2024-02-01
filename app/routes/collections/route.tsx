94b8e6b9fbaffd2b92499c1bc67142be85ca9630
Kevin Rutherford <kevin@rutherford-software.com>
2024-02-01 11:18:08 +0000

    Fetch the hardcoded data from the BFF

--- app/routes/collections/route.tsx
+++ app/routes/collections/route.tsx
@@ -1,31 +1,34 @@
 import type { MetaFunction } from '@remix-run/node';
-import { Collection } from './collection';
 import { CollectionCard } from './collection-card';
+import { json } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
 
 export const meta: MetaFunction = () => [
   { title: 'Rookery' },
   { name: 'description', content: 'Rookery' },
 ];
 
+export const loader = async () => json([
+  {
+    name: 'CHS',
+    description: 'Papers under review by the CHS project',
+    papersCount: 12,
+    commentsCount: 23,
+    followersCount: 4,
+    lastActivityAt: '4 hours ago',
+  },
+  {
+    name: 'PRU3',
+    description: 'Papers to be referenced by the PRU3 project',
+    papersCount: 134,
+    commentsCount: 258,
+    followersCount: 11,
+    lastActivityAt: '3 days ago',
+  },
+]);
+
 export default function Collections() {
-  const colls: ReadonlyArray<Collection> = [
-    {
-      name: 'CHS',
-      description: 'Papers under review by the CHS project',
-      papersCount: 12,
-      commentsCount: 23,
-      followersCount: 4,
-      lastActivityAt: '4 hours ago',
-    },
-    {
-      name: 'PRU3',
-      description: 'Papers to be referenced by the PRU3 project',
-      papersCount: 134,
-      commentsCount: 258,
-      followersCount: 11,
-      lastActivityAt: '3 days ago',
-    },
-  ];
+  const colls = useLoaderData<typeof loader>();
 
   return (
     <div className='grow'>
