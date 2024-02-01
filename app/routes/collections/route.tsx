da3782a0e8ec69a8ebe55457cd3b93938451e709
Kevin Rutherford <kevin@rutherford-software.com>
2024-02-01 11:06:37 +0000

    Extract data from rendering in /collections

--- app/routes/collections/route.tsx
+++ app/routes/collections/route.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from '@remix-run/node';
+import { FC } from 'react';
 import { Metadatum } from './metadatum';
 
 export const meta: MetaFunction = () => [
@@ -6,33 +7,60 @@ export const meta: MetaFunction = () => [
   { name: 'description', content: 'Rookery' },
 ];
 
+type Collection = {
+  name: string,
+  description: string,
+  papersCount: number,
+  commentsCount: number,
+  followersCount: number,
+  lastActivityAt: string,
+};
+
+type CollectionCardProps = {
+  collection: Collection,
+};
+
+const CollectionCard: FC<CollectionCardProps> = (props: CollectionCardProps) => (
+  <div className='bg-white p-4'>
+    <h2 className='font-semibold text-teal-700 mb-4'>{props.collection.name}</h2>
+    <p className='mb-4'>{props.collection.description}</p>
+    <ul className='flex justify-between'>
+      <li><Metadatum>{props.collection.papersCount} papers</Metadatum></li>
+      <li><Metadatum>{props.collection.commentsCount} comments</Metadatum></li>
+      <li><Metadatum>{props.collection.followersCount} followers</Metadatum></li>
+      <li><Metadatum>{props.collection.lastActivityAt}</Metadatum></li>
+    </ul>
+  </div>
+);
+
 export default function Collections() {
+  const colls: ReadonlyArray<Collection> = [
+    {
+      name: 'CHS',
+      description: 'Papers under review by the CHS project',
+      papersCount: 12,
+      commentsCount: 23,
+      followersCount: 4,
+      lastActivityAt: '4 hours ago',
+    },
+    {
+      name: 'PRU3',
+      description: 'Papers to be referenced by the PRU3 project',
+      papersCount: 134,
+      commentsCount: 258,
+      followersCount: 11,
+      lastActivityAt: '3 days ago',
+    },
+  ];
+
   return (
     <div className='grow'>
       <ul className='overflow-y-auto'>
         <li className='border-b border-teal-500'>
-          <div className='bg-white p-4'>
-            <h2 className='font-semibold text-teal-700 mb-4'>CHS</h2>
-            <p className='mb-4'>Papers under review by the CHS project.</p>
-            <ul className='flex justify-between'>
-              <li><Metadatum>12 papers</Metadatum></li>
-              <li><Metadatum>19 comments</Metadatum></li>
-              <li><Metadatum>4 followers</Metadatum></li>
-              <li><Metadatum>Last updated 4 hours ago</Metadatum></li>
-            </ul>
-          </div>
+          <CollectionCard collection={colls[0]} />
         </li>
         <li className='border-b border-teal-500'>
-          <div className='bg-white p-4'>
-            <h2 className='font-semibold text-teal-700 mb-4'>PRU3</h2>
-            <p className='mb-4'>Papers to be referenced by the PRU3 project.</p>
-            <ul className='flex justify-between'>
-              <li><Metadatum>134 papers</Metadatum></li>
-              <li><Metadatum>258 comments</Metadatum></li>
-              <li><Metadatum>11 followers</Metadatum></li>
-              <li><Metadatum>Last updated 3 days ago</Metadatum></li>
-            </ul>
-          </div>
+          <CollectionCard collection={colls[1]} />
         </li>
       </ul>
     </div>
