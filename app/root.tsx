5c4e983ae01b8f6a894d9f1e9824f3e5b184a8e7
Kevin Rutherford <kevin@rutherford-software.com>
2024-02-08 16:32:41 +0000

    Make the feed wrapper (more) reusable

--- app/root.tsx
+++ app/root.tsx
@@ -3,24 +3,18 @@ import {
   Links,
   LiveReload,
   Meta,
-  NavLink,
   Outlet,
   Scripts,
   ScrollRestoration,
 } from '@remix-run/react';
-import type { FC, ReactNode } from 'react';
 import stylesheet from '~/tailwind.css';
-import {
-  ClipboardDocumentListIcon,
-  Cog8ToothIcon,
-  UserGroupIcon,
-} from '@heroicons/react/24/solid';
+import { WithFeed } from './components/with-feed';
 
 export const links: LinksFunction = () => [
   { rel: 'stylesheet', href: stylesheet },
 ];
 
-type FeedEvent = {
+export type FeedEvent = {
   initials: string;
   userName: string;
   userHandle: string;
@@ -31,7 +25,7 @@ type FeedEvent = {
 
 type FEProps = FeedEvent;
 
-function FeedEventCard(props: FEProps) {
+export function FeedEventCard(props: FEProps) {
   return (
     <div className='bg-white p-4'>
       <div className='flex justify-between mb-4'>
@@ -114,28 +108,6 @@ const feedData: ReadonlyArray<FeedEvent> = [
   },
 ];
 
-type ColumnProps = {
-  children: ReactNode;
-};
-
-const Column: FC<ColumnProps> = (props: ColumnProps) => (
-  <div className='flex flex-col border-solid border border-teal-500 rounded-xl overflow-hidden'>{props.children}</div>
-);
-
-type ColumnTitleProps = {
-  title: string;
-  icon: React.ComponentType<{
-    className?: string;
-  }>;
-};
-
-const ColumnTitle: FC<ColumnTitleProps> = (props: ColumnTitleProps) => (
-  <div className='shrink-0 border-b border-teal-500 text-teal-700 p-3 font-semibold bg-teal-200'>
-    <props.icon className='h-6 w-6 inline mr-3' />
-    {props.title}
-  </div>
-);
-
 export default function App() {
   return (
     <html lang="en">
@@ -148,44 +120,9 @@ export default function App() {
       <body className="h-screen bg-zinc-100 text-zinc-600">
         <div className="min-h-screen h-full flex flex-col">
           <div className='container mx-auto my-12 h-full overflow-hidden'>
-            <div className='grid grid-cols-2 gap-12 h-full overflow-hidden'>
-              <Column>
-                <ColumnTitle title='Local timeline' icon={UserGroupIcon} />
-                <ul className='overflow-y-auto'>
-                  { feedData.map((event, ix) => (
-                    <li key={ix} className='border-b border-teal-500'>
-                      <FeedEventCard {...event} />
-                    </li>
-                  ))}
-                </ul>
-              </Column>
-              <Column>
-                <ColumnTitle title='Local collections' icon={ClipboardDocumentListIcon} />
-                <div className='flex flex-row h-full'>
-                  <Outlet />
-                  <div className='grow-0 bg-teal-200 border-l border-teal-500 text-teal-700 h-full'>
-                    <ul className='flex flex-col items-center px-2'>
-                      <li className='flex mt-6'>
-                        <NavLink
-                          className={({ isActive }) => isActive ? 'text-teal-200 shrink rounded-full bg-teal-700 block' : '' }
-                          to={'collections'}
-                        >
-                          <ClipboardDocumentListIcon className='h-10 w-10 p-2 inline' />
-                        </NavLink>
-                      </li>
-                      <li className='flex mt-6'>
-                        <NavLink
-                          className={({ isActive }) => isActive ? 'text-teal-200 shrink rounded-full bg-teal-700 block' : '' }
-                          to={'settings'}
-                        >
-                          <Cog8ToothIcon className='h-10 w-10 p-2 inline' />
-                        </NavLink>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </Column>
-            </div>
+            <WithFeed feedData={feedData}>
+              <Outlet />
+            </WithFeed>
           </div>
         </div>
         <ScrollRestoration />
