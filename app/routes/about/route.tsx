c42b899b120c26ec53c7c4c16d80e9b287d4526f
Kevin Rutherford <kevin@rutherford-software.com>
2024-02-17 11:32:30 +0000

    Fetch the content of the /about page from the backend

--- app/routes/about/route.tsx
+++ app/routes/about/route.tsx
@@ -9,23 +9,8 @@ type AboutResponse = {
 };
 
 export const loader = async () => {
-  const value: AboutResponse = {
-    type: 'About',
-    data: {
-      community: {
-        name: 'Health organisation, policy and economics (HOPE)',
-        affiliation: 'Centre for Primary Care and Health Services Research, Manchester University',
-        overview: `
-        This interdisciplinary theme focuses upon research which investigates the supply, organisation, management and financing of health and social care services.
-
-Our expertise encompasses rigorous econometric analysis and a wide range of qualitative social scientific methods, including particular experience in the use of ethnographic approaches to understand organisational processes.`,
-        admins: ['@DonnaB', '@kevin'],
-      },
-      backend: {
-        version: 'unknown',
-      },
-    },
-  };
+  const response = await fetch('http://backend:44002/about');
+  const value: AboutResponse = await response.json();
   return json(value.data);
 };
 
