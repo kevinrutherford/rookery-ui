c2383d9c0afc1663c886f0ac2c49e600e83cc170
Kevin Rutherford <kevin@rutherford-software.com>
2024-03-02 17:06:38 +0000

    Specify the complete route explicitly, to avoid confusion #58

--- app/routes/collections/collection-card.tsx
+++ app/routes/collections/collection-card.tsx
@@ -10,7 +10,7 @@ type CollectionCardProps = {
 
 export const CollectionCard: FC<CollectionCardProps> = (props: CollectionCardProps) => (
   <Card>
-    <Link to={props.collection.id} className='block hover:underline'>
+    <Link to={`/collections/${props.collection.id}`} className='block hover:underline'>
       <h2 className='font-semibold mb-4'>{props.collection.name}</h2>
       <p className='mb-4'>{props.collection.description}</p>
       <ul className='flex justify-between'>
