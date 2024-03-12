14700afd7d4fa2a3d333a4db1c83f5bfd8dc2499
Kevin Rutherford <kevin@rutherford-software.com>
2024-03-12 17:30:15 +0000

    Remove speculative content until the customer requests it #77

--- app/routes/collections_.$id/entry-card.tsx
+++ app/routes/collections_.$id/entry-card.tsx
@@ -11,7 +11,9 @@ type EntryCardProps = {
 export const EntryCard: FC<EntryCardProps> = (props: EntryCardProps) => (
   <Card>
     <Link to={`/entries/${props.entry.id}`} className='block hover:underline'>
-      <h2 className='font-semibold mb-4'>{props.entry.frontMatter ? props.entry.frontMatter.title : 'Title: Unknown at present'}</h2>
+      {(props.entry.frontMatter) && (
+        <h2 className={'font-semibold mb-4'}>props.entry.frontMatter.title</h2>
+      )}
       <p className='mb-4'>doi: {props.entry.doi}</p>
     </Link>
   </Card>
