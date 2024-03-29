d8f4711ba66cdb5e737d4e76848c985874db732d
Kevin Rutherford <kevin@rutherford-software.com>
2024-03-29 16:17:04 +0000

    Clean up vertical spacing #93

--- app/routes/collections_.$id/render-page-content.tsx
+++ app/routes/collections_.$id/render-page-content.tsx
@@ -7,7 +7,7 @@ export const renderPageContent = (collection: CollectionResource): ReactNode => 
   <div className='flex flex-col overflow-hidden'>
     <div className='flex flex-col bg-white mb-4 p-4 rounded-md overflow-hidden'>
       <p className='font-semibold'>{collection.attributes.name}</p>
-      <p className='mb-4'>{collection.attributes.description}</p>
+      <p className='mb-8'>{collection.attributes.description}</p>
       <ul className='overflow-y-auto mb-4'>
         { collection.entries.map((entry) => (
           <li key={entry.id} className='mb-4'>
