9899577378dd8623f634d68b74f5c75997d8f855
Kevin Rutherford <kevin@rutherford-software.com>
2024-04-07 18:30:31 +0100

    Display entry titles in the collection #93

--- app/routes/collections_.$id/entry-card.tsx
+++ app/routes/collections_.$id/entry-card.tsx
@@ -4,6 +4,16 @@ import ReactTimeAgo from 'react-time-ago'
 import { EntryResource } from '~/api-resources/entry'
 import { WorkResource } from '~/api-resources/work'
 
+const title = (work: WorkResource) => {
+  switch (work.attributes.crossrefStatus) {
+    case 'not-determined':
+    case 'not-found':
+      return `doi: ${work.id}`
+    case 'found':
+      return work.attributes.title
+  }
+}
+
 type EntryCardProps = {
   collectionid: string,
   entry: EntryResource,
@@ -13,7 +23,7 @@ type EntryCardProps = {
 export const EntryCard: FC<EntryCardProps> = (props: EntryCardProps) => (
   <div className='bg-slate-100 mb-4 p-4 rounded-md hover:shadow-lg'>
     <Link to={`/entries/${props.entry.id}`} className='block hover:underline'>
-      <p className='mb-4'>doi: {props.work.id}</p>
+      <p className='mb-4'>{title(props.work)}</p>
       <div className='text-sm text-slate-500 flex justify-between'>
         <span>{props.entry.attributes.commentsCount} comments</span>
         <div>
