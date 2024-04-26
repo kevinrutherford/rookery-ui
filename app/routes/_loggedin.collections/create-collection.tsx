f6d5136757f3155f7d6d25aa8fc04a52d1573d19
Kevin Rutherford <kevin@rutherford-software.com>
2024-04-26 12:16:04 +0100

    Reuse the same component for all text form fields #122

--- app/routes/_loggedin.collections/create-collection.tsx
+++ app/routes/_loggedin.collections/create-collection.tsx
@@ -1,6 +1,7 @@
 import { useFetcher } from '@remix-run/react'
 import { FC } from 'react'
 import { Card } from '~/components/card'
+import { TextField } from '~/components/forms'
 
 export const CreateCollection: FC = () => {
   const fetcher = useFetcher()
@@ -8,21 +9,7 @@ export const CreateCollection: FC = () => {
     <Card>
       <h3 className='font-semibold mb-6'>Create a new collection:</h3>
       <fetcher.Form method="post" action="/collections" className="w-full">
-        <div className="md:flex md:items-center mb-4">
-          <div className="md:w-1/3">
-            <label className="block text-slate-500 md:text-right md:mb-0 pr-4" htmlFor="name">
-              Name
-            </label>
-          </div>
-          <div className="md:w-2/3">
-            <input
-              id="name"
-              name="name"
-              type="text"
-              className="bg-slate-100 appearance-none border-2 border-slate-100 rounded w-full py-2 px-4 text-slate-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-            />
-          </div>
-        </div>
+        <TextField label='Name' attr='name' />
         <div className="md:flex md:items-center mb-4">
           <div className="md:w-1/3">
             <label className="block text-slate-500 md:text-right md:mb-0 pr-4" htmlFor="description">
@@ -37,21 +24,7 @@ export const CreateCollection: FC = () => {
             />
           </div>
         </div>
-        <div className="md:flex md:items-center mb-4">
-          <div className="md:w-1/3">
-            <label className="block text-slate-500 md:text-right mb-1 md:mb-0 pr-4" htmlFor="handle">
-              Handle
-            </label>
-          </div>
-          <div className="md:w-2/3">
-            <input
-              id="handle"
-              name="handle"
-              type="text"
-              className="bg-slate-100 appearance-none border-2 border-slate-100 rounded w-full py-2 px-4 text-slate-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-            />
-          </div>
-        </div>
+        <TextField label='Handle' attr='handle' />
         <div className="md:flex md:items-center">
           <div className="md:w-1/3"></div>
           <div className="md:w-2/3">
