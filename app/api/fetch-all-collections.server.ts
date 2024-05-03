a86fd056ef3e630369e315846e6c8afecd2b2a21
Kevin Rutherford <kevin@rutherford-software.com>
2024-05-03 11:56:57 +0100

    Authenticate when fetching all collections from the API #143

--- app/api/fetch-all-collections.server.ts
+++ app/api/fetch-all-collections.server.ts
@@ -1,5 +1,11 @@
 export const fetchAllCollections = async () => {
-  const response = await fetch('http://views:44002/collections')
+  const response = await fetch('http://views:44002/collections', {
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${process.env.DEVELOPMENT_BEARER_TOKEN}`,
+    },
+  })
   return await response.json()
 }
 
