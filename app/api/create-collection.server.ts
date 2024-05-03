7d143c7fefdbd529e08dcb7af89a1323e2ac17fa
Kevin Rutherford <kevin@rutherford-software.com>
2024-05-03 12:09:46 +0100

    Authenticate when creating a collection #143

--- app/api/create-collection.server.ts
+++ app/api/create-collection.server.ts
@@ -2,7 +2,10 @@ export const createCollection = async (formData: FormData) => {
   const updates = Object.fromEntries(formData)
   await fetch('http://commands:44001/collections', {
     method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${process.env.DEVELOPMENT_BEARER_TOKEN}`,
+    },
     body: JSON.stringify({
       ...updates,
       id: updates.handle,
