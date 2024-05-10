bca2eb0e31a33b5692fa67356714d18e40f3a680
Kevin Rutherford <kevin@rutherford-software.com>
2024-05-10 11:42:11 +0100

    Add a login form with very simple authentication for now #143

---
+++ app/routes/login/route.tsx
@@ -0,0 +1,43 @@
+import type { ActionFunctionArgs, LoaderFunctionArgs } from '@remix-run/node'
+import { Form } from '@remix-run/react'
+import { Card } from '~/components/card'
+import { SubmitButton, TextField } from '~/components/forms'
+import { authenticator } from '~/services/auth.server'
+
+export default function LoginScreen() {
+  return (
+    <Card>
+      <h3>Log in</h3>
+      <Form method="post">
+        <TextField label='Username' attr='username' />
+        <div className="md:flex md:items-center mb-4">
+          <div className="md:w-1/3">
+            <label className="block text-slate-500 md:text-right md:mb-0 pr-4" htmlFor='password'>
+            Password
+            </label>
+          </div>
+          <div className="md:w-2/3">
+            <input type='password' name='password' required
+              className="bg-slate-100 appearance-none border-2 border-slate-100 rounded w-full py-2 px-4 text-slate-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+            />
+          </div>
+        </div>
+        <SubmitButton label='Log in' />
+      </Form>
+    </Card>
+  )
+}
+
+export async function action({ request }: ActionFunctionArgs) {
+  return await authenticator.authenticate('user-pass', request, {
+    successRedirect: '/',
+    failureRedirect: '/login',
+  })
+};
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  return await authenticator.isAuthenticated(request, {
+    successRedirect: '/',
+  })
+};
+
---
+++ app/services/auth.server.ts
@@ -0,0 +1,28 @@
+import { Authenticator } from 'remix-auth'
+import { FormStrategy } from 'remix-auth-form'
+import invariant from 'tiny-invariant'
+import { sessionStorage } from '~/services/session.server'
+
+export const authenticator = new Authenticator<string>(sessionStorage)
+
+// The rest of the code above here...
+
+authenticator.use(
+  new FormStrategy(async ({ form }) => {
+    const username = form.get('username')
+    const password = form.get('password')
+
+    invariant(typeof username === 'string', 'username must be a string')
+    invariant(username.length > 0, 'username must not be empty')
+
+    invariant(typeof password === 'string', 'password must be a string')
+    invariant(password.length > 0, 'password must not be empty')
+
+    if (username === process.env.DEVELOPMENT_USERNAME)
+      return username
+
+    throw new Error('Unknown username')
+  }),
+  'user-pass',
+)
+
