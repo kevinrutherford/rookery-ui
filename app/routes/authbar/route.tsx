b8da644f8d5a18958393b90ea9828a62365a3cc9
Kevin Rutherford <kevin@rutherford-software.com>
2024-05-20 16:29:01 +0100

    Provide a redirect path on login #143

--- app/routes/authbar/route.tsx
+++ app/routes/authbar/route.tsx
@@ -1,5 +1,5 @@
 import { ActionFunctionArgs } from '@remix-run/node'
-import { Form, Link } from '@remix-run/react'
+import { Form, Link, useLocation } from '@remix-run/react'
 import { FC } from 'react'
 import { Container } from '~/components/container'
 import { SubmitButton } from '~/components/forms'
@@ -13,19 +13,22 @@ type Props = {
   username?: string,
 }
 
-export const AuthBar: FC<Props> = (props: Props) => (
-  <div className='mb-12 pt-4 pb-4 overflow-hidden bg-slate-100'>
-    <Container>
-      {
-        (props.username)
-          ? (
-            <Form method='post' action='/authbar'>
-              <SubmitButton label='Logout' />
-            </Form>
-          )
-          : <Link to='/login'>Log in</Link>
-      }
-    </Container>
-  </div>
-)
+export const AuthBar: FC<Props> = (props: Props) => {
+  const location = useLocation()
+  return (
+    <div className='mb-12 pt-4 pb-4 overflow-hidden bg-slate-100'>
+      <Container>
+        {
+          (props.username)
+            ? (
+              <Form method='post' action='/authbar'>
+                <SubmitButton label='Logout' />
+              </Form>
+            )
+            : <Link to={`/login?returnTo=${location.pathname}`}>Log in</Link>
+        }
+      </Container>
+    </div>
+  )
+}
 
