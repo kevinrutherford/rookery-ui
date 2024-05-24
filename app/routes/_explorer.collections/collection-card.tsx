aa25a5361527d6d175d39ff3184eb61c4d7846a1
Kevin Rutherford <kevin@rutherford-software.com>
2024-05-24 11:29:02 +0100

    Add a hidden privacy icon to all collection cards #143

--- app/routes/_explorer.collections/collection-card.tsx
+++ app/routes/_explorer.collections/collection-card.tsx
@@ -1,3 +1,4 @@
+import { EyeSlashIcon } from '@heroicons/react/24/outline'
 import { Link } from '@remix-run/react'
 import { FC } from 'react'
 import { CollectionResource } from '~/api-resources/collection'
@@ -10,7 +11,10 @@ type CollectionCardProps = {
 export const CollectionCard: FC<CollectionCardProps> = (props: CollectionCardProps) => (
   <Card>
     <Link to={`/collections/${props.collection.id}`} className='block hover:underline'>
-      <h2 className='font-semibold mb-4'>{props.collection.attributes.name}</h2>
+      <div className='flex justify-between mb-4'>
+        <h2 className='font-semibold'>{props.collection.attributes.name}</h2>
+        { false && <EyeSlashIcon className='h-5 w-5 pl-1 inline' /> }
+      </div>
       <p className='mb-4'>{props.collection.attributes.description}</p>
     </Link>
   </Card>
