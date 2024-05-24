79fdc80964c18020ae3606783b85723d49eb750b
Kevin Rutherford <kevin@rutherford-software.com>
2024-05-24 11:15:24 +0100

    Remove some obsolete icons #143

--- app/components/content-nav-items.tsx
+++ app/components/content-nav-items.tsx
@@ -1,22 +1,15 @@
-import { InformationCircleIcon } from '@heroicons/react/24/outline'
-import { ClipboardDocumentListIcon } from '@heroicons/react/24/solid'
-import type { ReactNode } from 'react'
-
 type NavItem = {
   title: string,
-  icon: ReactNode,
   route: string,
 }
 
 export const contentNavItems: Record<string, NavItem> = {
   collections: {
     title: 'Local collections',
-    icon: <ClipboardDocumentListIcon className='h-10 w-10 p-2 inline' />,
     route: 'collections',
   },
   about: {
     title: 'About',
-    icon: <InformationCircleIcon className='h-10 w-10 p-2 inline' />,
     route: 'about',
   },
 }
