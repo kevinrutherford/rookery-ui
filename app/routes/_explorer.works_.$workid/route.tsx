aef0a5c3995fc0e6fd4558279874f5c283d5aa4a
Kevin Rutherford <kevin@rutherford-software.com>
2024-07-10 18:55:15 +0100

    Display all details of a Work when available

--- app/routes/_explorer.works_.$workid/route.tsx
+++ app/routes/_explorer.works_.$workid/route.tsx
@@ -1,3 +1,4 @@
+import { ArrowTopRightOnSquareIcon } from '@heroicons/react/24/outline'
 import { json, LoaderFunctionArgs } from '@remix-run/node'
 import { useLoaderData  } from '@remix-run/react'
 import { pipe } from 'fp-ts/lib/function.js'
@@ -5,12 +6,40 @@ import * as t from 'io-ts'
 import invariant from 'tiny-invariant'
 import * as api from '~/api'
 import { parse } from '~/api-resources/parse'
-import { workResource } from '~/api-resources/work'
+import { WorkResource, workResource } from '~/api-resources/work'
 import { Card } from '~/components/card'
 
 const workResponse = t.type({
   data: workResource,
 })
+
+const header = (work: WorkResource) => {
+  switch (work.attributes.crossrefStatus) {
+    case 'not-determined':
+      return (
+        <>
+          <p className='mb-4 font-semibold'>{work.attributes.doi}</p>
+          <p>Searching...</p>
+        </>
+      )
+    case 'not-found':
+      return (
+        <>
+          <p className='mb-4 font-semibold'>{work.attributes.doi}</p>
+          <p>This doesn&apos;t seem to be a valid DOI.</p>
+        </>
+      )
+    case 'found':
+      return (
+        <>
+          <p className='mb-4 font-semibold'>{work.attributes.title}</p>
+          <p className='mb-4'>{work.attributes.authors.join(', ')}</p>
+          <p className='mb-4 font-semibold'>Abstract</p>
+          <p className='mb-4'>{work.attributes.abstract}</p>
+        </>
+      )
+  }
+}
 
 export const loader = async ({ request, params }: LoaderFunctionArgs) => {
   invariant(params.workid, 'workid must be supplied')
@@ -27,7 +56,16 @@ export default function WorkDetails() {
   return (
     <div className='flex flex-col overflow-hidden'>
       <div className='flex flex-col bg-white mb-4 p-4 rounded-md overflow-hidden'>
-        <p className='mb-4 font-semibold'>{response.data.attributes.doi}</p>
+        { header (response.data) }
+        <div className='flex justify-between mb-4'>
+          <span>DOI: {response.data.attributes.doi}</span>
+          <a className='block hover:underline'
+            href={`https://doi.org/${response.data.attributes.doi}`}
+            target='_blank' rel="noreferrer"
+          >
+            Original document <ArrowTopRightOnSquareIcon className='h-5 w-5 pl-1 pb-1 inline' />
+          </a>
+        </div>
       </div>
     </div>
   )
