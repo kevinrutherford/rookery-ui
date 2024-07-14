8e8e21136e1adc8364f03b5554c2d64c87f5e09d
Kevin Rutherford <kevin@rutherford-software.com>
2024-07-14 14:28:32 +0100

    Introduce menu items for all feeds

--- app/routes/_explorer/route.tsx
+++ app/routes/_explorer/route.tsx
@@ -31,11 +31,18 @@ const ExplorerLayout = () => {
           <Column>
             <ul className='p-4 bg-slate-100 mb-4 rounded-md'>
               <li className='inline mr-6 mt-6 mb-6'>
-                <NavLink
-                  className='border-b-4 border-slate-400'
-                  to={location.pathname}
-                >
-                  Local timeline
+                <NavLink className='border-slate-400' to={location.pathname}>
+                  Following
+                </NavLink>
+              </li>
+              <li className='inline mr-6 mt-6 mb-6'>
+                <NavLink className='border-b-4 border-slate-400' to={location.pathname}>
+                  Local
+                </NavLink>
+              </li>
+              <li className='inline mr-6 mt-6 mb-6'>
+                <NavLink className='border-slate-400' to={location.pathname}>
+                  Federated
                 </NavLink>
               </li>
             </ul>
--- app/routes/localtimeline/update-card.tsx
+++ app/routes/localtimeline/update-card.tsx
@@ -1,4 +1,4 @@
-import {Link} from '@remix-run/react'
+import { Link } from '@remix-run/react'
 import ReactTimeAgo from 'react-time-ago'
 import { UpdateResource } from '~/api-resources/update'
 import { WorkResource } from '~/api-resources/work'
