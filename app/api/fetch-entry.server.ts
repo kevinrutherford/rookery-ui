ca73078a3b292db89a32da05057eab79c3fb8d18
Kevin Rutherford <kevin@rutherford-software.com>
2024-07-16 07:57:10 +0100

    Request the authors of comments

--- app/api/fetch-entry.server.ts
+++ app/api/fetch-entry.server.ts
@@ -1,7 +1,7 @@
 import { get } from './get.server'
 
 export const fetchEntry = async (entryId: string, request: Request) => {
-  const response = await get(`/entries/${entryId}?include=collection,comments,work`, request)
+  const response = await get(`/entries/${entryId}?include=collection,comments,comments.author,work`, request)
   return await response.json()
 }
 
