fb303eb42e1bf43b0101804fb3e677673fd46791
Kevin Rutherford <kevin@rutherford-software.com>
2024-07-31 19:27:53 +0100

    Delete an unused method

--- app/components/timeline-page.ts
+++ app/components/timeline-page.ts
@@ -1,6 +1,3 @@
-import { pipe } from 'fp-ts/lib/function.js'
-import * as O from 'fp-ts/lib/Option.js'
-import * as RA from 'fp-ts/lib/ReadonlyArray.js'
 import { CommunityResource } from '~/api-resources/community'
 import { EntryResource } from '~/api-resources/entry'
 import { MemberResource } from '~/api-resources/member'
@@ -11,24 +8,10 @@ import { LocalTimelineResponse } from '~/routes/localtimeline/route'
 export class TimelinePage {
   readonly updates: ReadonlyArray<UpdateResource>
   readonly includes: ReadonlyArray<MemberResource | CommunityResource | EntryResource | WorkResource>
-  readonly community: O.Option<CommunityResource>
 
   constructor(response: LocalTimelineResponse) {
     this.updates = response.data
     this.includes = response.included
-    this.community = pipe(
-      response.included,
-      RA.filter((inc): inc is CommunityResource => inc.type === 'community'),
-      RA.head,
-    )
-  }
-
-  communityName() {
-    return pipe(
-      this.community,
-      O.getOrElseW(() => { throw new Error('No community included with local timeline') }),
-      (community) => community.attributes.name,
-    )
   }
 
 }
