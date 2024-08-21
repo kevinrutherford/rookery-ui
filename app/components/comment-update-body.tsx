66ee5c8956177f3c94658a1dc00cc7a81a4f108b
Kevin Rutherford <kevin@rutherford-software.com>
2024-08-21 17:16:29 +0100

    Display the discussion title instead of calculating it again

--- app/components/comment-update-body.tsx
+++ app/components/comment-update-body.tsx
@@ -2,7 +2,6 @@ import { FC } from 'react'
 import { EntryResource } from '~/api-resources/entry'
 import { RelatedResources } from '~/api-resources/related-resources'
 import { InboxUpdateCommentCreated, UpdateCommentCreated } from '~/api-resources/update'
-import { WorkResource } from '~/api-resources/work'
 import { InternalLink } from './internal-link'
 import { lookupResource } from './lookup-resource'
 import { PaperTitle } from './paper-title'
@@ -13,21 +12,11 @@ type Props = {
 }
 
 export const CommentUpdateBody: FC<Props> = (props: Props) => {
-  const entry = lookupResource(props.related, props.update.relationships.entry) as EntryResource
-  const work = lookupResource(props.related, props.update.relationships.work) as WorkResource
-  let title: string
-  switch (work.attributes.crossrefStatus) {
-    case 'found':
-      title = work.attributes.title
-      break
-    default:
-      title = work.attributes.doi
-      break
-  }
+  const discussion = lookupResource(props.related, props.update.relationships.entry) as EntryResource
   return (
     <div>
-      Commented on <InternalLink to={`/discussions/${entry.id}`}>
-        <PaperTitle text={title} />
+      Commented on <InternalLink to={`/discussions/${discussion.id}`}>
+        <PaperTitle text={discussion.attributes.title} />
       </InternalLink>
     </div>
   )
