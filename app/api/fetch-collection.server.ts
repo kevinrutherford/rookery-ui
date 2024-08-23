90f81220326e1748b74925407b85b23f26f6c7ca
Kevin Rutherford <kevin@rutherford-software.com>
2024-08-23 11:31:13 +0100

    Use current domain language for discussions

--- app/api/fetch-collection.server.ts
+++ app/api/fetch-collection.server.ts
@@ -1,7 +1,7 @@
 import { get } from './get.server'
 
 export const fetchCollection = async (id: string, request: Request) => {
-  const response = await get(`/collections/${id}?include=entries,entries.work`, request)
+  const response = await get(`/collections/${id}?include=discussions,discussions.work`, request)
   return await response.json()
 }
 
