7e6a5c0d9d97b0a480ca59c680af3ce8345991fc
Kevin Rutherford <kevin@rutherford-software.com>
2024-08-29 07:24:03 +0100

    Add missing file

---
+++ app/api-resources/updates/discussion-started.ts
@@ -0,0 +1,21 @@
+import * as t from 'io-ts'
+import * as tt from 'io-ts-types'
+import { collectionIdentifier } from '../collection'
+import { discussionIdentifier } from '../discussion'
+import { memberIdentifier } from '../member'
+import { workIdentifier } from '../work'
+
+export const updateDiscussionStarted = t.type({
+  type: t.literal('update:discussion-started'),
+  id: t.string,
+  attributes: t.type({
+    occurred_at: tt.DateFromISOString,
+  }),
+  relationships: t.type({
+    actor: t.type({ data: memberIdentifier }),
+    collection: t.type({ data: collectionIdentifier }),
+    discussion: t.type({ data: discussionIdentifier }),
+    work: t.type({ data: workIdentifier }),
+  }),
+})
+
